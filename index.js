dc88b5c0f19d0d5ce43cc42307cf1b113f841d7f
alina12g <alina12g@yahoo.com>
2021-12-05 11:22:53 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,93 @@
+const container = document.getElementById("container");
+
+const h1 = document.createElement("h1");
+h1.textContent = "Guess the number";
+container.appendChild(h1);
+
+const input = document.createElement("input");
+input.setAttribute("type", "number");
+input.onchange = "limitInputNum(this)";
+input.className = "input";
+input.addEventListener("change", function () {
+  let value = parseInt(this.value);
+  if (value < 1) this.value = 1;
+  if (value > 20) this.value = 20;
+});
+container.appendChild(input);
+
+const button = document.createElement("button");
+button.textContent = "Let's guess";
+button.className = "btn";
+container.appendChild(button);
+
+button.addEventListener("submit", function (e) {
+  e.preventDefault();
+});
+
+function limitInputNum(input) {
+  if (input.value < 1) input.value = 1;
+  if (input.value > 20) input.value = 20;
+}
+
+function randomInput() {
+  return Math.floor(Math.random() * 20) + 1;
+}
+const randomNumber = randomInput();
+document.getElementsByClassName("input").innerHTML = randomInput;
+//the guess
+
+const div = document.createElement("div");
+div.id = "div";
+
+const h2 = document.createElement("h2");
+h2.textContent = "This is my guess";
+div.appendChild(h2);
+
+let guessedNumber = document.createElement("input");
+guessedNumber.className = "input-bottom";
+guessedNumber.defaultValue = 0;
+div.appendChild(guessedNumber);
+
+document.body.appendChild(div);
+const divBtns = document.createElement("div");
+divBtns.id = "btnsDiv";
+
+const correct = document.createElement("button");
+correct.textContent = "Correct";
+correct.className = "btn-correct";
+correct.id = "clickCorrect";
+divBtns.appendChild(correct);
+
+correct.onclick = CounterClicks;
+
+let countCorrect = 0;
+function CounterClicks() {
+  countCorrect += 1;
+  clickCorrect.innerHTML = countCorrect;
+}
+
+const wrong = document.createElement("button");
+wrong.textContent = "Wrong";
+wrong.className = "btn-wrong";
+wrong.id = "clickWrong";
+divBtns.appendChild(wrong);
+
+wrong.onclick = CounterW;
+
+let countWrong = 0;
+function CounterW() {
+  countWrong += 1;
+  clickWrong.innerHTML = countWrong;
+}
+
+document.body.appendChild(divBtns);
+
+// function guessNum(a, b) {
+//   numA = document.getElementsByClassName(a).value;
+//   numB = document.getElementsByClassName(b).value;
+//   if (numA === numB) {
+//     return "It took me {CounterC} guesses to have it right.";
+//   } else if (numA != numB) {
+//     return "You lie, the numbers match, it took me {CounterW} guesses";
+//   }
+// }
